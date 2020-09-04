e61e1e42b6cc61e30e96765bd774ff593b1c7956
Adjéri SABI <kod047@gmail.com>
2020-09-05 06:31:33 +0900

    http package and data fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:networking_with_flutter/parsing_json/json_parsing.dart';
 import 'package:networking_with_flutter/ui/home.dart';
 
 void main() {
-  runApp(Home());
+  runApp(new MaterialApp(home: JsonParsingSimple()));
 }
