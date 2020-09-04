48a32658120e60e7e5034814afb19dfddea09bcd
Adjéri SABI <kod047@gmail.com>
2020-09-05 07:17:58 +0900

    PODO used for listing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:networking_with_flutter/parsing_json/json_parsing.dart';
+import 'package:networking_with_flutter/parsing_json/json_parsing_map.dart';
 import 'package:networking_with_flutter/ui/home.dart';
 
 void main() {
-  runApp(new MaterialApp(home: JsonParsingSimple()));
+  runApp(new MaterialApp(home: JsonParsingMap()));
 }
